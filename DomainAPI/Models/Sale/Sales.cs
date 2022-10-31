9bebeedf0b156dab8661c97bd5753262f6dc4f00
Julio Jr <zejulio_@hotmail.com>
2022-10-31 00:50:27 -0300

    Sales_3.0vs

--- DomainAPI/Models/Sale/Sales.cs
+++ DomainAPI/Models/Sale/Sales.cs
@@ -4,12 +4,15 @@ using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace DomainAPI.Models.Sale
-{
-    public class Sales
-    {
+namespace DomainAPI.Models.Sale {
+    public class Sales {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
         public Flights Flight { get; set; }
         public List<Passengers> Passengers { get; set; }
+
         public bool Reserved { get; set; }
         public bool Sold { get; set; }
     }
--- Saler/Controllers/ConsumerController.cs
+++ Saler/Controllers/ConsumerController.cs
@@ -12,28 +12,24 @@ namespace Saler.Controllers
 {
     public class ConsumerController
     {
-        private readonly string _consumerGetFligth = "https://localhost:44330/api/Flights/";
+        private readonly string _consumerGetFligth = "https://localhost:44330/api/Flights/GetOneFlight/";
         private readonly string _consumerPutFligth = "https://localhost:44330/api/Flights/cancelflight/";
         private readonly string _consumerGetPassenger = "https://localhost:44388/api/Passenger/StatusValids/Cpf?cpf=";
-        public async Task<Flights> GetFlightAsync(DateTime date, string rab)
-        {
-            using (HttpClient _adressClient = new())
-            {
-                HttpResponseMessage response = await _adressClient.GetAsync(_consumerGetFligth + date + "/" + rab);
+        public async Task<Flights> GetFlightAsync(DateTime date, string rab) {
+            using (HttpClient _adressClient = new()) {
+                HttpResponseMessage response = await _adressClient.GetAsync(_consumerGetFligth + date.ToString("yyyy-MM-dd  HH:mm:ss") + "/" + rab);
                 response.EnsureSuccessStatusCode();
                 var json = await response.Content.ReadAsStringAsync();
                 return JsonConvert.DeserializeObject<Flights>(json);
             }
         }
 
-        public async Task<Flights> PutFlightAsync(Flights flight)
-        {
-            using (HttpClient _adressClient = new())
-            {
+        public async Task<Flights> PutFlightAsync(Flights flight) {
+            using (HttpClient _adressClient = new()) {
                 string fligthPost = JsonConvert.SerializeObject(flight);
                 HttpResponseMessage response = await _adressClient.GetAsync(_consumerPutFligth + fligthPost);
                 response.EnsureSuccessStatusCode();
-                return flight;  
+                return flight;
             }
         }
 
