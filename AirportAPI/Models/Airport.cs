9594e2ef733e0e07ceee5e80554bc03722878351
Felipe Mascarin <felipe_mascarin@hotmail.com>
2022-11-23 11:27:46 -0300

    Versão entrega

--- AirportAPI/Models/Airport.cs
+++ AirportAPI/Models/Airport.cs
@@ -1,7 +1,7 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using System.Text.Json.Serialization;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using Newtonsoft.Json;
-using System.Text.Json.Serialization;
 
 namespace AirportAPI.Models
 {
